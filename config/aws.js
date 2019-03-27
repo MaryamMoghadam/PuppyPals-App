48718d4f74635199d0de43f0d271f4daa00bbd32
unknown <m.moghadam@knights.ucf.edu>
2019-03-27 19:08:19 -0400

    uploading config folder

---
+++ config/aws.js
@@ -0,0 +1,6 @@
+const aws = {
+	secretKey: 'YourOwnAWS3SecretKey',
+	accessKey: 'YourOwnAWS3AccessKey'
+}
+
+module.exports = aws
---
+++ config/firebase.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+  apiKey: "YourOwnFirebaseAPIKey",
+  authDomain: "YourOwnFirebaseauthDomain",
+  databaseURL: "YourOwnFirebasedatabaseURL",
+  projectId: "YourOwnFirebaseprojectId",
+  storageBucket: "OPTIONAL",
+  messagingSenderId: "YourOwnFirebasemessagingSenderId"
+};
+
+module.exports = firebaseConfig;
